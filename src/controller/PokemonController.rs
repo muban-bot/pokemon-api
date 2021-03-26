2ef7fd596b24c0114db01997eb220abec2087c37
Felix Angell <felixangell@otzaf.org>
2021-03-26 18:10:11 +0000

    add deps to tokio + warp; simple hello world server running

---
+++ src/controller/PokemonController.rs
@@ -0,0 +1,7 @@
+struct PokemonController {
+
+}
+
+fn foo() {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("pokemon" / String)
+        .map(|name| format!("Hello {}!", name));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 5000))
+        .await;
 }
